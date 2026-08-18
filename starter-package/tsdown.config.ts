8fb1b1469ce70753e76a946f6fda9e4d1bc808a7
Jiakun Zhao <jiakun.zhao@outlook.com>
2026-08-18 22:43:51 +0800

    chore: update deps

--- starter-package/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  entry: 'src/index.ts',
-  alias: {
-    '~': fileURLToPath(new URL('./src', import.meta.url)),
-  },
-})
